73ad7183c13b174e22d59110ce85dd2b8574a732
James C. Tucker <jamesctucker@protonmail.com>
2020-05-01 00:19:11 -0500

    added hero image to header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,3 +1,31 @@
-<h1>National Park Finder</h1>
+<div class="hero-image">
+    <div class="hero-text">
+        <h1>National Park Finder</h1>
+    </div>
+</div>
 
-<style></style>
+
+
+<style>
+
+.hero-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7)), url('https://www.nationalgeographic.com/content/dam/expeditions/destinations/north-america/private/Yosemite/Hero-Yosemite.ngsversion.1524840074980.adapt.1900.1.jpg');
+  height: 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+
+
+
+</style>
